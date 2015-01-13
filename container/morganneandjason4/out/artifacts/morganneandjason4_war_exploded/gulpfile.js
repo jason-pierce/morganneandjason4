e8683be1bb81540d4ecc7273f8e012f52893c0da
Jason Pierce <jason.pierce@agilex.com>
2015-01-13 12:08:36 -0500

    adding bower components to git

--- container/morganneandjason4/out/artifacts/morganneandjason4_war_exploded/gulpfile.js
+++ container/morganneandjason4/out/artifacts/morganneandjason4_war_exploded/gulpfile.js
@@ -1,11 +1,11 @@
 /* jshint node:true */
 'use strict';
-// generated on 2014-12-28 using generator-gulp-webapp 0.2.0
+// generated on 2014-12-29 using generator-gulp-webapp 0.2.0
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function () {
-  return gulp.src('styles/main.scss')
+  return gulp.src('app/styles/main.scss')
     .pipe($.plumber())
     .pipe($.rubySass({
       style: 'expanded',
@@ -16,7 +16,7 @@ gulp.task('styles', function () {
 });
 
 gulp.task('jshint', function () {
-  return gulp.src('scripts/**/*.js')
+  return gulp.src('app/scripts/**/*.js')
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
     .pipe($.jshint.reporter('fail'));
@@ -26,21 +26,21 @@ gulp.task('html', ['styles'], function () {
   var lazypipe = require('lazypipe');
   var cssChannel = lazypipe()
     .pipe($.csso)
-    .pipe($.replace, '../../bower_components/bootstrap-sass-official/assets/fonts/bootstrap','fonts');
-  var assets = $.useref.assets({searchPath: '{.tmp,../web}'});
+    .pipe($.replace, 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap','fonts');
+  var assets = $.useref.assets({searchPath: '{.tmp,app}'});
 
-  return gulp.src('*.jsp')
+  return gulp.src('app/*.html')
     .pipe(assets)
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', cssChannel()))
     .pipe(assets.restore())
     .pipe($.useref())
-    .pipe($.if('*.jsp', $.minifyHtml({conditionals: true, loose: true})))
+    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('images', function () {
-  return gulp.src('images/**/*')
+  return gulp.src('app/images/**/*')
     .pipe($.cache($.imagemin({
       progressive: true,
       interlaced: true
@@ -49,7 +49,7 @@ gulp.task('images', function () {
 });
 
 gulp.task('fonts', function () {
-  return gulp.src(require('main-bower-files')().concat('fonts/**/*'))
+  return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
     .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
     .pipe($.flatten())
     .pipe(gulp.dest('dist/fonts'));
@@ -57,9 +57,8 @@ gulp.task('fonts', function () {
 
 gulp.task('extras', function () {
   return gulp.src([
-    '*.*',
-    '!*.jsp',
-    '!*.html',
+    'app/*.*',
+    '!app/*.html',
     '../../node_modules/apache-server-configs/dist/.htaccess'
   ], {
     dot: true
@@ -74,11 +73,11 @@ gulp.task('connect', ['styles'], function () {
   var app = require('connect')()
     .use(require('connect-livereload')({port: 35729}))
     .use(serveStatic('.tmp'))
-    .use(serveStatic('../web'))
+    .use(serveStatic('app'))
     // paths to bower_components should be relative to the current file
     // e.g. in app/index.html you should use ../bower_components
-    .use('../../bower_components', serveStatic('bower_components'))
-    .use(serveIndex('../web'));
+    .use('/bower_components', serveStatic('bower_components'))
+    .use(serveIndex('app'));
 
   require('http').createServer(app)
     .listen(9000)
@@ -95,13 +94,13 @@ gulp.task('serve', ['connect', 'watch'], function () {
 gulp.task('wiredep', function () {
   var wiredep = require('wiredep').stream;
 
-  gulp.src('styles/*.scss')
+  gulp.src('app/styles/*.scss')
     .pipe(wiredep())
-    .pipe(gulp.dest('styles'));
+    .pipe(gulp.dest('app/styles'));
 
-  gulp.src('*.jsp')
+  gulp.src('app/*.html')
     .pipe(wiredep({exclude: ['bootstrap-sass-official']}))
-    .pipe(gulp.dest('../web'));
+    .pipe(gulp.dest('app'));
 });
 
 gulp.task('watch', ['connect'], function () {
@@ -109,14 +108,13 @@ gulp.task('watch', ['connect'], function () {
 
   // watch for changes
   gulp.watch([
-    '*.html',
-    '*.jsp',
+    'app/*.html',
     '.tmp/styles/**/*.css',
-    'scripts/**/*.js',
-    'images/**/*'
+    'app/scripts/**/*.js',
+    'app/images/**/*'
   ]).on('change', $.livereload.changed);
 
-  gulp.watch('styles/**/*.scss', ['styles']);
+  gulp.watch('app/styles/**/*.scss', ['styles']);
   gulp.watch('bower.json', ['wiredep']);
 });
 
