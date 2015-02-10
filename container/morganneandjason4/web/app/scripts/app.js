3c67278d395cab9e204ac013f6f657308810263a
Jason Pierce <jason.pierce@agilex.com>
2015-02-10 12:03:50 -0500

    initial Polymer build, with layout scaffolding

--- container/morganneandjason4/web/app/scripts/app.js
+++ container/morganneandjason4/web/app/scripts/app.js
@@ -2,8 +2,21 @@
   'use strict';
 
   document.addEventListener('polymer-ready', function () {
-    // Perform some behaviour
-    console.log('Polymer is ready to rock!');
+
+
+    var toolbarBottom = document.querySelector('core-toolbar div.bottom'),
+        headerTitle = document.querySelector('core-toolbar div.top'),
+        navIcon = document.querySelector('#navicon'),
+        mquery = document.querySelector('#mquery'),
+        contentSmall = document.querySelector('#content-small'),
+        contentLarge = document.querySelector('#content-large');
+    mquery.addEventListener('core-media-change', function() {
+      toolbarBottom.hidden = mquery.queryMatches;
+      navIcon.hidden = !mquery.queryMatches;
+      headerTitle.hidden = !mquery.queryMatches;
+      contentSmall.hidden = !mquery.queryMatches;
+      contentLarge.hidden = mquery.queryMatches;
+    });
   });
 
 // wrap document so it plays nice with other libraries
